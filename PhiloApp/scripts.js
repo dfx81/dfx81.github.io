2de6a7f9809787edb9273f8a2492068641a9eaca
dfx81 <danialfitrighazali@gmail.com>
2020-05-20 15:26:04 +0800

    Proper checkbox for PhiloApp

--- PhiloApp/scripts.js
+++ PhiloApp/scripts.js
@@ -35,7 +35,7 @@ function showPolicy(){
        str += "2. By agreeing to this policy and using this program/website or the services provided by us, we're allowed to collect data from the user. The private data collected includes the user's email address as required by the registration, and other personal data that can be found from the email.\n";
        str += "3. By agreeing to this policy and using our services, you're also approving the way we handles your personal data, which is by selling them to third parties. We're NOT reliable for any damages done to the user, such as identity theft, and spam emails.\n";
        str += "4. The user will also be able to subscribe to our newsletter, where we occasionally send offers regarding our service. In context of the current service, the latest leaderboard position will be mailed to the respective users. The user CANNOT unsubscribe from our newsletter.\n";
-       str += "5. By agreeing to this, the user agrees that they're not responsible with their personal data and will freely give them out to other parties such as this our obvious trap website.\n";
+       str += "5. By agreeing to this, the user agrees that they're not responsible with their personal data and will freely give them out to other parties such as this obvious trap website.\n";
        str += "6. Referring to point 5, the user agrees to be refered as 'dummy dumb dumb' or 'boomer'.\n";
     
     readTime.push(Date.now());
@@ -58,33 +58,37 @@ function accept(tricked){
     let understand = document.getElementById("understand");
     let cookies = document.getElementById("cookies");
     
-    hide("form");
-    hide("policy");
-    show("result");
-    
-    let data = document.getElementById("data");
-    let str = "Your Result:<br/><br/>";
-    str += "Tricked to accept ridiculous privacy policy?: " + tricked + "<br/>";
-    str += "Checked the privacy policy?: " + check + "<br/>";
-    str += "Number of times read the policy: " + nRead + " times<br/>";
-    
-    let longest = 0;
-    for (i = 0; i != nRead; i++)
-        if (readTime[i] > longest)
-            longest = readTime[i];
-    
-    str += "Time taken to read the policy: (longest) " + longest + " seconds<br/>";
-    if (tricked)
+    if (agree.checked && cookies.checked || !tricked)
     {
-        str += "Agreed to ridiculous policies?: " + agree.value + "<br/>";
-        str += "Did not join the newsletter that can't be unsubscribed from?: " + news.value + "<br/>";
-        str += "Admit did not read the policy?: " + read.value + "<br/>";
-        str += "Admit the existence of a point that did not exist in the policy?: " + understand.value + "<br/>";
-        str += "Accept cookies?: " + cookies.value + "<br/>";
-    }
+        hide("form");
+        hide("policy");
+        show("result");
     
-    data.innerHTML = str;
-    window.open("https://youtu.be/dQw4w9WgXcQ");
+        let data = document.getElementById("data");
+        let str = "Your Result:<br/><br/>";
+        str += "Tricked to accept ridiculous privacy policy?: " + tricked + "<br/>";
+        str += "Checked the privacy policy?: " + check + "<br/>";
+        str += "Number of times read the policy: " + nRead + " times<br/>";
+    
+        let longest = 0;
+        for (i = 0; i != nRead; i++)
+            if (readTime[i] > longest)
+                longest = readTime[i];
+        
+        str += "Time taken to read the policy: (longest) " + longest + " seconds<br/>";
+        if (tricked)
+        {
+            str += "Agreed to ridiculous policies?: " + agree.checked + "<br/>";
+            str += "Did not join the newsletter that can't be unsubscribed from?: " + news.checked + "<br/>";
+            str += "Admit did not read the policy?: " + read.checked + "<br/>";
+            str += "Admit the existence of a point that did not exist in the policy?: " + understand.checked + "<br/>";
+            str += "Accept cookies?: " + cookies.checked + "<br/><br/>";
+            str += "Guess we found the dummy dumb dumb user...<br/>";
+        }
+        
+        data.innerHTML = str;
+        window.open("https://youtu.be/dQw4w9WgXcQ");
+    }
 }
 
 function decline(){
