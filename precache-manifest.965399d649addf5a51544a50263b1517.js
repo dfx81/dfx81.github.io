d1754dc54adcb1cd6f494b89c80b82d7d103a682
dfx81 <danialfitrighazali@gmail.com>
2020-07-12 17:57:17 +0800

    Updated layout

--- precache-manifest.965399d649addf5a51544a50263b1517.js
+++ precache-manifest.965399d649addf5a51544a50263b1517.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16330d8a6f3c0b286c8dc9dab5d27461",
+    "revision": "61d41f483c7c6bd46d746a9df9838992",
     "url": "/index.html"
   },
   {
-    "revision": "c83bae9baa81feb64952",
-    "url": "/static/css/main.cee48bc3.chunk.css"
+    "revision": "ec90f6f91c95e0ba9bcd",
+    "url": "/static/css/main.650ac677.chunk.css"
   },
   {
     "revision": "9bbdfac81252c307667e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.a576af00.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c83bae9baa81feb64952",
-    "url": "/static/js/main.7d4f3381.chunk.js"
+    "revision": "ec90f6f91c95e0ba9bcd",
+    "url": "/static/js/main.5490d2c1.chunk.js"
   },
   {
     "revision": "39ce663ce569126f5183",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fd9bc33cc9aa09fb45913bef63defd2.js"
+  "/precache-manifest.965399d649addf5a51544a50263b1517.js"
 );
 
 self.addEventListener('message', (event) => {
