7596a17249e865ef6fabee0031476a080d09ac41
dfx81 <danialfitrighazali@gmail.com>
2024-04-17 11:57:39 +0800

    Optimization for lighthouse

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 window.onload = () => {
     console.log("Website programmed & designed by Danial 'dfx' Fitri.");
     console.log("Site v1.0");
+
+    let mail = document.querySelector(".email > .small");
+
+    mail.innerText = mail.innerText.replace(" [plus] ", "+").replace(" [at] ", "@").replace(" [dot] ", ".").replace("spam", "dfx");
+    mail.setAttribute("href", "mailto:" + mail.innertext);
+
+    document.querySelector(".content").classList.add("slide-in");
+    document.querySelector(".header").classList.add("fade-in");
+    document.querySelector(".footer").classList.add("fade-in");
 };
